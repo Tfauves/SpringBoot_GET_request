2eed440a9260b004032c72dd72c1c104cb57ece7
Tfauves <tfauvelle@gmail.com>
2021-10-27 16:07:04 -0400

    initial commmit.

---
+++ src/main/java/com/careerdevs/GET/request/practice/GetRequestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.GET.request.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetRequestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetRequestPracticeApplication.class, args);
+	}
+
+}
