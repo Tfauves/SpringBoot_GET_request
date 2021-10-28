4cecb8223109d7e150943e492dfddc6c0d4f1627
Tfauves <tfauvelle@gmail.com>
2021-10-27 21:12:58 -0400

    refactor fields add toString.

--- src/main/java/com/careerdevs/GET/request/practice/employee/Employee.java
+++ src/main/java/com/careerdevs/GET/request/practice/employee/Employee.java
@@ -33,4 +33,45 @@ public class Employee {
     }
 
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
