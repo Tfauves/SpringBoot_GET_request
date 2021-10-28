66e3f3f4325ddf8531bf86d1f809ea9de5ccae36
Tfauves <tfauvelle@gmail.com>
2021-10-27 21:02:22 -0400

    employee package and employee class.

---
+++ src/main/java/com/careerdevs/GET/request/practice/employee/Employee.java
@@ -0,0 +1,18 @@
+package com.careerdevs.GET.request.practice.employee;
+
+//Employee should have minimum id, firstName, lastName
+//        4 routes Minimum
+//        root route
+//        get dummy employee route
+//        query param route to "create" employee
+//        path Param route using one field to return an employee.
+
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastname;
+    private String email;
+
+
+
+}
