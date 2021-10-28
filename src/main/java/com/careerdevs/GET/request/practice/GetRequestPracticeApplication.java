59dfac2c97e2f0041e84118848c3d2454a96d89b
Tfauves <tfauvelle@gmail.com>
2021-10-28 11:29:03 -0400

    controller defined.

--- src/main/java/com/careerdevs/GET/request/practice/GetRequestPracticeApplication.java
+++ src/main/java/com/careerdevs/GET/request/practice/GetRequestPracticeApplication.java
@@ -2,12 +2,9 @@ package com.careerdevs.GET.request.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-;
 
-import java.util.List;
 
 @SpringBootApplication
-
 public class GetRequestPracticeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/careerdevs/GET/request/practice/employee/EmployeeController.java
+++ src/main/java/com/careerdevs/GET/request/practice/employee/EmployeeController.java
@@ -4,8 +4,9 @@ package com.careerdevs.GET.request.practice.employee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
+
+
 
 @RestController
 @RequestMapping(path = "api/v1/employee")
